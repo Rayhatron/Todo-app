b6a1c7b54629393fe96adbb0a8c5e57d382702f7
Rayhatron <rayhatron@gmail.com>
2016-12-07 13:48:40 +0200

    changed route url

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -18,7 +18,7 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
-app.get('/todo', function(req, res){
+app.get('/', function(req, res){
 
     //get data from mlab database and send to view
     Todo.find({}, function(err, data){
@@ -28,7 +28,7 @@ app.get('/todo', function(req, res){
     
 });
 
-app.post('/todo', urlencodedParser, function(req, res){
+app.post('/', urlencodedParser, function(req, res){
     //get data from view and send to mlab nongodb
     var newTodo = Todo(req.body).save(function(err, data){
         if(err) throw err;
@@ -37,7 +37,7 @@ app.post('/todo', urlencodedParser, function(req, res){
     
 });
 
-app.delete('/todo/:item', function(req, res){
+app.delete('/:item', function(req, res){
     //delete requested item from mlab mongodb
 
     Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
